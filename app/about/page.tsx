8448c9ef3b9282433d0d5502b462d73b1b57adaa
muhammadanuslodhi <157319063+muhammadanuslodhi@users.noreply.github.com>
2026-07-18 03:34:15 +0500

    ourstory

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,13 @@
+
+
 import AboutHero from "../Components/abouthero";
+import OurStory from "../Components/ourstory";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutHero />
-      {/* baaki sections yahan add karein */}
+      <OurStory />
     </main>
   );
 }
